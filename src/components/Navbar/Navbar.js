d237a845354d51dd876a4f54da031561773d2be5
Muhammad Ahmed <iamahmedameer@gmail.com>
2021-08-16 13:57:17 +0100

    added mobile navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Navbar.css";
 import { FiSearch, FiHeart, FiShoppingCart } from "react-icons/fi";
 
 const Navbar = () => {
+  const [hamburgerState, setHamburgerState] = useState(false);
+  const onHamburgerClick = () => {
+    setHamburgerState(!hamburgerState);
+  };
+
   return (
     <div className="navbar">
       <h3>YOUR LOGO</h3>
@@ -27,6 +32,27 @@ const Navbar = () => {
           <li>
             <FiShoppingCart className="icon cart-icon" />
           </li>
+        </ul>
+      </div>
+      <div
+        onClick={onHamburgerClick}
+        className={hamburgerState ? "hamburger click-animation" : "hamburger"}
+      >
+        <div className="line-1"></div>
+        <div className="line-2"></div>
+      </div>
+      <div
+        className={
+          hamburgerState ? "mobile-menu mobile-menu-active" : "mobile-menu"
+        }
+      >
+        <ul>
+          <li>HOME</li>
+          <li>SHOP</li>
+          <li>LOOKBOOK</li>
+          <li>FEATURES</li>
+          <li>PAGES</li>
+          <li>BLOG</li>
         </ul>
       </div>
     </div>
